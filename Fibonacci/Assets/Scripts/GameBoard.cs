54ff3f3141a46f92da2958b80816f04c14ae1a77
Svetlana <s.lana.doroshenko@gmail.com>
2020-08-21 14:57:31 +0300

    Added swipe controller.

--- Fibonacci/Assets/Scripts/GameBoard.cs
+++ Fibonacci/Assets/Scripts/GameBoard.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System;
 using UnityEngine;
 
 public class GameBoard : MonoBehaviour
@@ -9,37 +12,112 @@ public class GameBoard : MonoBehaviour
     public GameObject background;
     public int width = 4;
     public int height = 4;
+    private int stepX = 0;
+    private int stepY = 0;
 
-    private GameBoardTile[,] boardPieces;
-    public GameBoardTile[,] BoardPieces { get { return boardPieces; } }
-    private ActiveTile[,] activeTiles;
-    public ActiveTile[,] ActiveTiles { get { return activeTiles; } }
+    public GameBoardTile[,] BoardPieces { get; private set; }
+    public ActiveTile[,] ActiveTiles { get; private set; }
+    private Point[, ] boardMask;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(background);
         // TODO: change it to be just an image and use only active tiles
-        boardPieces = new GameBoardTile[width, height];
+        BoardPieces = new GameBoardTile[width, height];
+        boardMask = new Point[width, height];
 
         for (int x = 0; x < width; ++x)
         {
             for (int y = 0; y < height; ++y)
             {
-                boardPieces[x, y] = generator.GenerateTile(x, y);
+                BoardPieces[x, y] = generator.GenerateTile(x, y);
+                boardMask[x, y] = new Point(-1, -1);
             }
         }
 
-        activeTiles = new ActiveTile[width, height];
+        ActiveTiles = new ActiveTile[width, height];
 
         for (int x = 0; x < width; ++x)
         {
             for (int y = 0; y < height; ++y)
             {
-                bool revealed = (x == 0 && y == 0); 
-                activeTiles[x, y] = generator.GenerateActiveTile(x, y, revealed);
+                bool revealed = (x == 0 && y == 0);
+                //Debug.Log(revealed);
+                ActiveTiles[x, y] = generator.GenerateActiveTile(x, y, revealed);
+                if (revealed)
+                {
+                    boardMask[x, y].X = x;
+                    boardMask[x, y].Y = y;
+                }
             }
         }
+    }
+
+    private void SetStep()
+    {
+        if (SwipeController.swipeDirection == Swipe.Down)
+        {
+            stepX = 0;
+            stepY = -1;
+        }
+        else if (SwipeController.swipeDirection == Swipe.Up)
+        {
+            stepX = 0;
+            stepY = 1;
+        }
+        else if (SwipeController.swipeDirection == Swipe.Left)
+        {
+            stepX = -1;
+            stepY = 0;
+        }
+        else if (SwipeController.swipeDirection == Swipe.Right)
+        {
+            stepX = 1;
+            stepY = 0;
+        }
+        else
+        {
+            stepX = 0;
+            stepY = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(SwipeController.swipeDirection);
+        SetStep();
+
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                if (ActiveTiles[x, y].revealed)
+                {
+                    Vector3 desiredPosition = ActiveTiles[x, y].transform.position 
+                        + new Vector3(stepX * (int)Math.Round(ActiveTiles[x, y].transform.localScale.x),
+                        stepY * (int)Math.Round(ActiveTiles[x, y].transform.localScale.y), 0);
+                    desiredPosition.x = Math.Max(desiredPosition.x, 0);
+                    desiredPosition.x = Math.Min(desiredPosition.x, width - 1);
+                    desiredPosition.y = Math.Max(desiredPosition.y, 0);
+                    desiredPosition.y = Math.Min(desiredPosition.y, height - 1);
+
+                    if (boardMask[(int)(desiredPosition.x), (int)(desiredPosition.y)].X == -1 && 
+                        boardMask[(int)(desiredPosition.x), (int)(desiredPosition.y)].Y == -1)
+                    {
+                        boardMask[(int)(ActiveTiles[x, y].transform.position.x),
+                                  (int)(ActiveTiles[x, y].transform.position.y)].X = -1;
+                        boardMask[(int)(ActiveTiles[x, y].transform.position.x),
+                                  (int)(ActiveTiles[x, y].transform.position.y)].Y = -1;
+                        ActiveTiles[x, y].transform.position = desiredPosition;
+                        boardMask[(int)(desiredPosition.x), (int)(desiredPosition.y)].X = x;
+                        boardMask[(int)(desiredPosition.x), (int)(desiredPosition.y)].Y = y;
+                    }
+                }
+            }
+        }
+
     }
 
 }
---
+++ Fibonacci/Assets/Scripts/SwipeController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Swipe { None, Up, Down, Left, Right };
+
+public class SwipeController : MonoBehaviour
+{
+    public float minSwipeLength = 20f;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+
+    public static Swipe swipeDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Detecting swipe");
+        DetectSwipe();
+    }
+
+    public void DetectSwipe()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Debug.Log("Touch");
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                EstimateSwipeDirection();
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("press");
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //Debug.Log("release");
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+            EstimateSwipeDirection();
+        }
+        else
+        {
+            swipeDirection = Swipe.None;
+        }
+    }
+
+    private void EstimateSwipeDirection()
+    {
+        // Make sure it was a legit swipe, not a tap
+        if (currentSwipe.magnitude < minSwipeLength)
+        {
+            swipeDirection = Swipe.None;
+            return;
+        }
+
+        //Debug.Log(currentSwipe.magnitude);
+        currentSwipe.Normalize();
+        //Debug.Log(currentSwipe.x + " " + currentSwipe.y);
+
+        // Swipe up
+        if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+            swipeDirection = Swipe.Up;
+            // Swipe down
+        }
+        else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+            swipeDirection = Swipe.Down;
+            // Swipe left
+        }
+        else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+        {
+            swipeDirection = Swipe.Left;
+            // Swipe right
+        }
+        else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+        {
+            swipeDirection = Swipe.Right;
+        }
+    }
+}
